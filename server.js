117626e51dd38f0d16b663b5789a85e2ba3c83b7
RaghavaD542 <raghavadonthoju@gmail.com>
2023-07-30 21:50:27 +0530

    updated

--- server.js
+++ server.js
@@ -26,6 +26,21 @@ const con = mysql.createConnection({
   database: process.env.DB_DBNAME || 'signup',
 });
 
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ Error: 'You are no Authenticated' });
+  } else {
+    jwt.verify(token, 'jwt-secret-key', (err, decoded) => {
+      // console.log(decoded.id);
+      if (err) return res.json({ Error: 'Token wrong' });
+      req.role = decoded.role;
+      req.id = decoded.id;
+      next();
+    });
+  }
+};
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/images');
@@ -58,7 +73,67 @@ app.get('/getEmployee', (req, res) => {
   });
 });
 
-app.get('/get/:id', (req, res) => {
+app.get('/getTasks', (req, res) => {
+  const disableSafeModeQuery = 'SET SQL_SAFE_UPDATES = 0';
+  const createTemporaryTableQuery = `
+    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_tasks AS
+    SELECT tasks.id, employee.name AS assigned_to, employee.status AS new_status
+    FROM tasks
+    INNER JOIN employee ON employee.work = tasks.taskname`;
+  const updateTasksQuery = `
+    UPDATE tasks
+    INNER JOIN tmp_tasks ON tasks.id = tmp_tasks.id
+    SET tasks.assigned_to = tmp_tasks.assigned_to, tasks.status = tmp_tasks.new_status`;
+  const dropTemporaryTableQuery = 'DROP TEMPORARY TABLE IF EXISTS tmp_tasks';
+  const enableSafeModeQuery = 'SET SQL_SAFE_UPDATES = 1';
+
+  con.query(disableSafeModeQuery, (err) => {
+    if (err) {
+      console.log('Error disabling safe mode:', err.message);
+      return res.json({ Error: 'Error disabling safe mode' });
+    }
+
+    con.query(createTemporaryTableQuery, (err) => {
+      if (err) {
+        console.log('Error creating temporary table:', err.message);
+        return res.json({ Error: 'Error creating temporary table' });
+      }
+
+      con.query(updateTasksQuery, (err) => {
+        if (err) {
+          console.log('Error updating tasks:', err.message);
+          return res.json({ Error: 'Error updating tasks' });
+        }
+
+        con.query(dropTemporaryTableQuery, (err) => {
+          if (err) {
+            console.log('Error dropping temporary table:', err.message);
+            return res.json({ Error: 'Error dropping temporary table' });
+          }
+
+          con.query(enableSafeModeQuery, (err) => {
+            if (err) {
+              console.log('Error enabling safe mode:', err.message);
+              return res.json({ Error: 'Error enabling safe mode' });
+            }
+
+            const getTasksQuery = 'SELECT * FROM tasks ORDER BY status';
+            con.query(getTasksQuery, (err, result) => {
+              if (err) {
+                console.log('Get Tasks error in SQL:', err.message);
+                return res.json({ Error: 'Get Tasks error in SQL' });
+              }
+
+              return res.json({ Status: 'Success', Result: result });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+app.get('/get/:id', verifyUser, (req, res) => {
   const id = req.params.id;
   const sql = 'SELECT * FROM employee where id = ?';
   con.query(sql, [id], (err, result) => {
@@ -105,20 +180,14 @@ app.delete('/delete/:id', (req, res) => {
     return res.json({ Status: 'Success' });
   });
 });
-
-const verifyUser = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.json({ Error: 'You are no Authenticated' });
-  } else {
-    jwt.verify(token, 'jwt-secret-key', (err, decoded) => {
-      if (err) return res.json({ Error: 'Token wrong' });
-      req.role = decoded.role;
-      req.id = decoded.id;
-      next();
-    });
-  }
-};
+app.delete('/deleteTask/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'Delete FROM tasks WHERE id = ?';
+  con.query(sql, [id], (err, result) => {
+    if (err) return res.json({ Error: 'delete task error in sql' });
+    return res.json({ Status: 'Success' });
+  });
+});
 
 app.get('/dashboard', verifyUser, (req, res) => {
   return res.json({ Status: 'Success', role: req.role, id: req.id });
@@ -154,7 +223,7 @@ app.post('/login', (req, res) => {
       return res.json({ Status: 'Error', Error: 'Error in runnig query' });
     if (result.length > 0) {
       const id = result[0].id;
-      const token = jwt.sign({ role: 'admin' }, 'jwt-secret-key', {
+      const token = jwt.sign({ role: 'admin', id: id }, 'jwt-secret-key', {
         expiresIn: '1d',
       });
       res.cookie('token', token);
@@ -179,9 +248,9 @@ app.post('/employeelogin', (req, res) => {
           if (response) {
             const token = jwt.sign(
               { role: 'employee', id: result[0].id },
-              'jwt-secret-key',
-              { expiresIn: '1d' }
+              'jwt-secret-key'
             );
+            // console.log(token);
             res.cookie('token', token);
             return res.json({ Status: 'Success', id: result[0].id });
           } else {
@@ -200,6 +269,7 @@ app.post('/employeelogin', (req, res) => {
 
 app.get('/employee/:id', (req, res) => {
   const id = req.params.id;
+  // console.log(id);
   const sql = 'SELECT * FROM employee where id = ?';
   con.query(sql, [id], (err, result) => {
     if (err) return res.json({ Error: 'Get employee error in sql' });
@@ -210,6 +280,13 @@ app.get('/employee/:id', (req, res) => {
 app.get('/logout', (req, res) => {
   res.clearCookie('token');
   return res.json({ Status: 'Success' });
+});
+app.post('/createTask', (req, res) => {
+  const sql = 'INSERT INTO `signup`.`tasks` (`taskname`) VALUES (?)';
+  con.query(sql, [req.body.name], (err, result) => {
+    if (err) return res.json({ Error: 'Inside createTask query' });
+    return res.json({ Status: 'Success' });
+  });
 });
 
 app.post('/create', upload.single('image'), (req, res) => {
